03bb24589ffc01a70d77244c8ac2651e0fd8a61d
9unn <99804619+9unn@users.noreply.github.com>
2024-03-13 16:47:19 +0700

    2

--- components/week1.js/Collections.js
+++ components/week1.js/Collections.js
@@ -1,39 +1,60 @@
-import React from 'react';
-import { Image, Text, View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { FlatList, Image, Text, View } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 
 
-export default function Collections() {
+export default function Collections(props) {
 
+    const [onlineReccommend, setOnlineReccommend] = useState([]);   
+
+    const loadOnlineReccommend = async () => {
+        try{
+            let promise = await fetch('https://raw.githubusercontent.com/9unn/FoodnavigatorApp/main/eiei.json');
+            let data = await promise.json();
+            console.log("Load Data : " , data);
+          //SET STATE
+          setOnlineReccommend(data);
+        }catch(error){
+            console.log("ERROR : " , error);
+        }
+    }  
+
+    useEffect(() => {
+        loadOnlineReccommend();
+    }, []);
+    
 
         return (
 
-            <View style={{ flex: 1, padding: 10}}>
-                <View style={{flexDirection: 'row'}}>
-                   <View>
+            <View style={props.style}>
+                <View style={{flexDirection: 'column'}}>
+                   <View style={{flexDirection: 'row'}}>
                         <Text style={{paddingLeft: 10, fontSize: 25, fontWeight: 'bold', marginTop: 85}}>Recommended for you</Text>
+                   </View>
+                   
+                   <View style={{flexDirection: 'row'}}>
                         <Text style={{padding: 10, fontSize: 18, color: 'gray'}}>Find the best Restaurant, Cafe and Bars </Text>
                    </View>
                    
                    <FlatList 
 
                         horizontal={true}
-                        // data={tours}
-                        data={onlineTours}
-                        
+                    
+                        data={onlineReccommend}
 
                         renderItem={
                             ({ item, index }) => {
                                 console.log(item, index, item.uri);
                                 return (
-                                    <View style={{marginRight: 10}}>
-                                        <Image style={{width : 200,height: 150, borderRadius: 10 }} source={{uri: item.uri}}/>
-                                        <View style={{ marginTop : -30, height : 30 , width: 200, paddingHorizontal : 10, backgroundColor : 'black', opacity : 0.5, borderBottomLeftRadius : 10 , borderBottomRightRadius : 10}}>
-                                            <Text style={{ fontSize:20, color : "white"}}>{item.title}</Text>
+                                
+                                    <View style={{marginTop: 20}}>
+                                        <Image style={{width : 300,height: 250, borderRadius: 1,marginRight: 20}} source={{uri: item.uri}}/>
 
-                                        </View>
-                                        
+                                        <View style={{ marginTop : -50, height : 30 , width: 300, backgroundColor : 'black', opacity : 0.5, borderBottomLeftRadius : 10 , borderBottomRightRadius : 10}}>
+                                            <Text style={{ fontSize:20, color : "white"}}>{item.title}</Text>
+                                        </View>   
                                     </View>
+                                    
                                 );
                             }
                         }
--- screens/week1.js/Find.js
+++ screens/week1.js/Find.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Image, ScrollView,  View } from 'react-native';
-import Home from '../../components/week1.js/Home.js';
-import Collections from '../../components/week1.js/Collections.js';
-import Menu from '../../components/week1.js/Menu.js';
+import Home from '../../components/week1.js/Home';
+import Collections from '../../components/week1.js/Collections';
+
 
 
 
@@ -12,8 +12,8 @@ export default function Find() {
             <View style={{ flex: 1, paddingTop: 20 , margin: 20}}>
 
                 <Home />
-                
                 <Collections/>
+                
                 
 
             </View>
